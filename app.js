9402ddafb1ba133f7659c788f83241a68d7ca6d7
belen-bai-21 <87877432+belen-bai-21@users.noreply.github.com>
2021-09-15 13:38:18 -0300

    Update app.js

--- app.js
+++ app.js
@@ -1,2 +1,24 @@
+const $=(id)=> document.getElementById(id)
 
+const actualizarTextos=()=>{
+$('top-text').textContent=$('top-text-input').value
+$('bottom-tex').texContent=$('bottom-text-input').value
+}
+const actualizarFuente=()=>{
+    const fuente = $('text-font-select').value
+
+    $('top-text').style.fontFamily=fuente
+    $('bottom.text').style.fontFamily=fuente
+}
+
+const incializarTexto=()=>{
+$('top-text-input').addEventListener('input',actualizarTextos)
+$('bottom-text-input').addEventListener('input',actualizarTextos)
+$('text-font-select').addEventListener('change',actualizarFuente)
+}
+
+const incializar=()=>{
+    incializarTexto()
+}
+window.onload=incializar
 
